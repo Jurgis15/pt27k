90b94322c798b65cea4232297ce0889dc02369bb
Jurgis <jurgisjakunas15@gmail.com>
2024-08-05 19:03:26 +0300

    lecture nr.2

---
+++ 2lecture.py
@@ -0,0 +1,47 @@
+
+# # Sukurkite programą, leidžiančią vartotojui įvesti savo vardą ir amžių.
+# name = input("Enter you name: ")
+# age = input("Enter your age: ")
+# now_age = 2024
+
+# birth_year = now_age - int(age)
+# print(birth_year) 
+
+
+# a = 5
+# b = "5"
+# Apskaičiuoti metus, kuriais vartotojas gimė.
+
+# a = 2024
+# b = 28
+# c = a - b 
+# print(c)
+# Išspausdinti atsakymą į terminalą.
+
+
+# Sukurti programą, leidžiančią vartotojui įvesti pilną sakinį.
+
+# sentence = input("enter your sentence:")
+
+# print(sentence[::-1])
+# išspausdinti sakinį atbuline tvarka.
+
+# print(sentence[0::2])
+# spausdinti kas antrą sakinio raidę.
+
+# nr1 = input("enter number no.1 ")
+# nr2 = input("enter number no.2 ")
+# a = 2
+# b = 4
+# c = a * b
+# print(c)
+# Sukurkite programą, kuri tikisi, kad naudotojas įves du skaičius.
+# padauginkite šiuos skaičius ir išspausdinkite atsakymą.
+
+skaicius1 = input()
+skaicius2 = input()
+skaicius3 = int(skaicius1) * int(skaicius2)
+print(skaicius3)
+
+# Sukurkite panašias programas su kitais ženklais.
+
