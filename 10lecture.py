4c0c2391b3ea86d96156c19e5cf879bb12ecd003
Jurgis <jurgisjakunas15@gmail.com>
2024-08-05 19:37:03 +0300

    all left files

---
+++ 10lecture.py
@@ -0,0 +1,24 @@
+# import random
+
+# def get_random_number():
+#     print(random.randint(0, 10))
+
+
+# get_random_number()
+
+
+def print_smth():
+    print("hey hou")
+
+print_smth()
+
+print("labas")
+# Patys sukurkite bent 5 skirtingas funkcijas ir jas išbandykite.
+
+
+# Sukurkite funkciją, kuri prie kiekvieno list nario prideda string galūnę.
+
+
+# Sukurkite mini python programą, kuri kaip įvestį paimtų du skaičius ir grąžintų jų sumą, atimtį, dalybą, daugybą.
+# Sukurkite funkciją, kuri grąžintų tik unikalius simbolius turinčias string reikšmes.
+# Parašykite programą, apibrėžiančią funkciją extract_email_addresses(), kuri priima tekstą kaip įvestį ir iš teksto ištraukia visus el. pašto adresus.
---
+++ condition.py
@@ -0,0 +1,31 @@
+# number_one = 500
+# number_two = 600
+# if number_one < number_two:
+#     print("number_one is greater than number_two!")
+# elif number_one == number_two:
+#     print("Numbers are equal !")
+
+#     number_one = 500
+# number_two = 600
+# if number_one < number_two :
+#     print("number_one is greater than number_two  !")
+# elif number_one == number_two :
+#     print("numbers are equal !")
+# else:
+#     print("number_two is greater than number_one !")
+
+# a = 200
+# b = 450
+# print("A") if a > b else print("B")
+
+# while True:
+#     user_input = input("Enter your name: ")
+#     if len(user_input) != 0:
+#         break
+# print(f"You entered {user_input }")
+
+# name = "Code Academy"
+# for character in name :
+#     print(character)
+
+
